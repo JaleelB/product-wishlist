aa7aa671bfb3a280f5e20815fce380c8d74846aa
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-23 08:46:51 -0400

    feat: homepage boilerplate

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,37 @@
+import FullNav from "@/components/full-nav";
+import Footer from "@/components/footer";
+import { Button } from "@/components/ui/button";
+import { Icons } from "@/components/icons";
+import Link from "next/link";
+
+export default function HomeLayout({
+    children,
+}: {
+    children: React.ReactNode;
+}) {
+  return (
+    <div className="flex h-screen flex-col">
+        <FullNav>
+            <div className="flex gap-2 items-center">
+                <Link href="https://github.com/JaleelB">
+                    <div className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 py-2 w-9 px-0">
+                        <Icons.github className="h-5 w-5" />
+                    </div>
+                </Link>
+                <Link href="https://twitter.com/jal_eelll">
+                    <div className="inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-9 py-2 w-9 px-0">
+                        <Icons.twitter className="h-5 w-5"/>
+                    </div>
+                </Link>
+                <Link href="/signin">
+                    <Button className="ml-3 px-6 rounded-full">Sign In</Button>
+                </Link>
+            </div>
+        </FullNav>
+        <main className="flex-grow w-screen">
+            {children}
+        </main>
+        <Footer/>
+    </div>
+  );
+}
---
+++ src/app/(home)/page.tsx
@@ -0,0 +1,10 @@
+
+export default function Home() {
+  return (
+    <section className="container h-full max-w-7xl mx-auto flex items-center justify-center">
+      <h1 className="text-5xl font-semibold text-center tracking-tight text-blacks sm:text-[5rem]">
+        Home
+      </h1>
+    </section>
+  );
+}

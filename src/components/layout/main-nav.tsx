48d2fb823a565897363adea04db5467638f67ce7
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-24 00:52:09 -0400

    feat: redirecting from public routes to homepage after auth

--- src/components/layout/main-nav.tsx
+++ src/components/layout/main-nav.tsx
@@ -1,13 +1,16 @@
+"use client"
 import Link from 'next/link';
-import React from 'react';
 import { Icons } from '../icons';
+import { useUser } from '@clerk/nextjs';
 
 export default  function MainNav({children}: {children: React.ReactNode}) {
 
+  const { isSignedIn } = useUser()
+
   return (
     <header className='w-screen border-b'> 
-      <div className='container max-w-[1400px] flex justify-between items-center h-16 py-6 px-4'>
-        <Link href="/">
+      <div className='container max-w-[1400px] flex justify-between items-center h-16 py-6'>
+        <Link href={isSignedIn ? "/home" : "/"}>
           <Icons.logo className="h-5 w-24"/>
         </Link>
         {children}
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,15 @@
 import { authMiddleware } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
 
 export default authMiddleware({
-  publicRoutes: ["/"]
+  publicRoutes: ["/"],
+  afterAuth:(auth, req) => {
+    // redirect them to dashboard home page
+    if(auth.userId &&  req.nextUrl.pathname === "/"){
+      const home = new URL('/home', req.url)
+      return NextResponse.redirect(home)
+    }
+  }
 });
 
 export const config = {

2cd3eca41f769c4ed6d94b467c2118ed24955745
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-26 21:37:05 -0400

    feat: character limit for wishlist input

--- src/schemas/wishlist.ts
+++ src/schemas/wishlist.ts
@@ -1,8 +1,8 @@
 import { z } from "zod";
 
 export const createWishlistSchema = z.object({
-    name: z.string().nonempty(),
-    description: z.string().nonempty(),
+    name: z.string().nonempty().max(60, "Name cannot exceed 60 characters"),
+    description: z.string().nonempty().max(160, "Description cannot exceed 160 characters"),
 });
 
 export const updateWishlistSchema = z.object({

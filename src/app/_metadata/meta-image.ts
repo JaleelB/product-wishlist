350dda4f94fe750a7001c8bfdb6118770c486843
Jaleel Bennett <jaleelbennett58@gmail.com>
2023-07-28 17:42:17 -0400

    update: added more image edge cases

--- src/app/_metadata/meta-image.ts
+++ src/app/_metadata/meta-image.ts
@@ -5,6 +5,12 @@ export const getMetaImage = (html: string): string => {
   const $ = cheerio.load(html);
   return $('meta[property="og:image"]').attr('content')!
     ?? $('meta[name="twitter:image"]').attr('content')
+    ?? $('input[name="productImageUrl"]').attr('value')
+    ?? $('#productImageUrl').attr('value')
+    ?? $('meta[itemprop="image"]').attr('content')
+    ?? $('article img[src]').attr('src')
+    ?? $('#content img[src]').attr('src')
+    ?? $('img[alt*="author" i]').attr('src')
     ?? $('.post-image').attr('src')
     ?? $('.entry-image').attr('src')
     ?? $('meta[property="og:image:secure_url"]').attr('content')
